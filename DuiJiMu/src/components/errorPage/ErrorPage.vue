bd75e86455e69b8f64662cc72dcf373f76161f8a
sunyan <sy2fmm@126.com>
2018-04-15 11:12:22 +0800

    处理错误页面

---
+++ DuiJiMu/src/components/errorPage/ErrorPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="error_contain">
+    <h1 class="error_logo">
+      <div class="error_msg">页面飞走啦</div>
+    </h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error_contain{
+  width: 100%;
+  height: 100%;
+  background: url("/source/error.png") repeat-x;
+
+}
+.error_contain .error_logo{
+
+}
+.error_contain .error_logo .error_msg{
+
+}
+</style>
--- DuiJiMu/src/router/index.js
+++ DuiJiMu/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ErrorPage from '@/components/errorPage/ErrorPage'
+import IndexWeather from '@/components/weatherKnow/indexWeather/IndexWeather'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/weather',
+      name: 'IndexWeather',
+      component: IndexWeather
+    },
+    {
+      path: '*',
+      name: 'ErrorPage',
+      component: ErrorPage
     }
   ]
 })
