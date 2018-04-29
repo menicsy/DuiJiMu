eb0661735a85b2b6b37eb698142140c985d1789b
sunyan <sy2fmm@126.com>
2018-04-29 19:52:00 +0800

    修复文件

--- DuiJiMu/src/router/index.js
+++ DuiJiMu/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ErrorPage from '@/components/errorPage/ErrorPage'
 import IndexWeather from '@/components/weatherKnow/indexWeather/IndexWeather'
 
 Vue.use(Router)
@@ -9,16 +8,8 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/weather',
+      path: '/indexweather',
       component: IndexWeather
-    },
-    {
-      path: '/errorpage',
-      component: ErrorPage
-    },
-    {
-      path: '*',
-      redirect: '/errorpage'
     }
   ]
 })
